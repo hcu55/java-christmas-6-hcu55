7848ce424a5706eda31fa9c819e03408503b04f4
ChanUi <hcu55@naver.com>
2023-11-12 16:08:35 +0900

    feat: 크리스마스 프로모션 시작 문구 출력 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+public class OutputView {
+    private static final String PRINT_CHRISTMAS_PROMOTION_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public void printChristmasPromotionStartMessage() {
+        System.out.println(PRINT_CHRISTMAS_PROMOTION_START_MESSAGE);
+    }
+}
