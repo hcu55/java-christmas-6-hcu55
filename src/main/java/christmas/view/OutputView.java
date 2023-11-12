3c4afbfeee93beff6616969ca9cb9215f7c681ec
ChanUi <hcu55@naver.com>
2023-11-12 16:20:38 +0900

    feat: 식당 예상 방문 날짜 입력 문구 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -2,8 +2,13 @@ package christmas.view;
 
 public class OutputView {
     private static final String PRINT_CHRISTMAS_PROMOTION_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String PRINT_EXPECTED_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
     public void printChristmasPromotionStartMessage() {
         System.out.println(PRINT_CHRISTMAS_PROMOTION_START_MESSAGE);
+    }
+
+    public void printExpectedVisitDateInputMessage() {
+        System.out.println(PRINT_EXPECTED_VISIT_DATE_MESSAGE);
     }
 }
