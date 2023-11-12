1a7593f362562f142259575e63d37738b3d54c4e
ChanUi <hcu55@naver.com>
2023-11-12 21:06:41 +0900

    feat: 주문 메뉴와 개수 입력 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,11 +1,27 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.Menu;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class InputView {
 
     public int inputExpectedVisitDate() {
         String inputVisitDate = Console.readLine();
         return Integer.parseInt(inputVisitDate);
+    }
+
+    public Map<Menu, Integer> inputOrderMenu() {
+        String inputMenu = Console.readLine();
+
+        return Arrays.stream(inputMenu.split(","))
+                .map(menu -> menu.split("-"))
+                .collect(Collectors.toMap(
+                        arr -> Menu.valueOf(arr[0]),
+                        arr -> Integer.parseInt(arr[1])
+                ));
     }
 }
