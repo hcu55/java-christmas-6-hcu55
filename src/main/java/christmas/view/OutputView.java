c98a09cbaec0137013ca6c9e9041cbb2be4f565b
ChanUi <hcu55@naver.com>
2023-11-12 20:49:28 +0900

    feat: 주문 메뉴와 개수 입력 문구 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,6 +3,7 @@ package christmas.view;
 public class OutputView {
     private static final String PRINT_CHRISTMAS_PROMOTION_START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String PRINT_EXPECTED_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String PRINT_ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     public void printChristmasPromotionStartMessage() {
         System.out.println(PRINT_CHRISTMAS_PROMOTION_START_MESSAGE);
@@ -10,5 +11,9 @@ public class OutputView {
 
     public void printExpectedVisitDateInputMessage() {
         System.out.println(PRINT_EXPECTED_VISIT_DATE_MESSAGE);
+    }
+
+    public void printOrderMenuInputMessage() {
+        System.out.println(PRINT_ORDER_MENU_MESSAGE);
     }
 }
