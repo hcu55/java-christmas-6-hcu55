366253a339415aafbbdae780cea462be22d9d51b
ChanUi <hcu55@naver.com>
2023-11-14 03:47:55 +0900

    feat: 크리스마스 디데이 할인 기능 추가

--- src/main/java/christmas/domain/ChristmasEvent.java
+++ src/main/java/christmas/domain/ChristmasEvent.java
@@ -3,10 +3,24 @@ package christmas.domain;
 import java.util.Map;
 
 public class ChristmasEvent {
+    private static final int CHRISTMAS_BASE_DISCOUNT = 1000;
+    private static final int INCREASE_DISCOUNT = 100;
+    private static final int CHRISTMAS_DAY = 25;
 
     private int calculateTotalOrderPrice(Map<Menu, Integer> order) {
         return order.entrySet().stream()
                 .mapToInt(entry -> entry.getKey().getPriceForCount(entry.getValue()))
                 .sum();
+    }
+
+    private int christmasDdayDiscount(int visitDate) {
+        if (isInChristmasDdayPeriod(visitDate)) {
+            return CHRISTMAS_BASE_DISCOUNT + INCREASE_DISCOUNT * (visitDate - 1);
+        }
+        return 0;
+    }
+
+    private boolean isInChristmasDdayPeriod(int visitDate) {
+        return visitDate <= CHRISTMAS_DAY;
     }
 }
