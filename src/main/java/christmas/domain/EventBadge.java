5d560c3802611f197f2b9a9f0c0a215c4402df7f
ChanUi <hcu55@naver.com>
2023-11-14 05:44:55 +0900

    feat: 총혜택 금액에 따른 이벤트 배지 부여 기능 구현

--- src/main/java/christmas/domain/EventBadge.java
+++ src/main/java/christmas/domain/EventBadge.java
@@ -1,7 +1,18 @@
 package christmas.domain;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class EventBadge {
     private static final int CHAMPAGNE_PRICE = 25_000;
+    private static final Map<Integer, String> badgeMap;
+
+    static {
+        badgeMap = new LinkedHashMap<>();
+        badgeMap.put(20_000, "산타");
+        badgeMap.put(10_000, "트리");
+        badgeMap.put(5_000, "별");
+    }
 
     public int totalBenefitAmount(int totalDiscount, boolean isChampagneGet) {
         int totalBenefit = totalDiscount;
@@ -9,5 +20,14 @@ public class EventBadge {
             totalBenefit += CHAMPAGNE_PRICE;
         }
         return totalBenefit;
+    }
+
+    public String GrantingEventBadge(int totalBenefit) {
+        for (Map.Entry<Integer, String> entry : badgeMap.entrySet()) {
+            if (totalBenefit >= entry.getKey()) {
+                return entry.getValue();
+            }
+        }
+        return "없음";
     }
 }
