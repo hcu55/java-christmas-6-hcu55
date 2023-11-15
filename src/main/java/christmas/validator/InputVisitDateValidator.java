3301df31b23b467b1ac65359d336ffbad3fd8436
ChanUi <hcu55@naver.com>
2023-11-15 13:24:17 +0900

    refactor: 식당 방문 날짜 예외처리 구조 리팩토링

--- src/main/java/christmas/validator/InputVisitDateValidator.java
+++ src/main/java/christmas/validator/InputVisitDateValidator.java
@@ -2,16 +2,15 @@ package christmas.validator;
 
 public class InputVisitDateValidator {
     private static final String NON_INPUT_ERROR_MESSAGE = "[ERROR] 입력하지 않으셨습니다. 예상 방문 날짜를 입력해주세요";
-    private static final String WRONG_RANGE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static final String NON_INTEGER_ERROR_MESSAGE = "[ERROR] 예상 방문 날짜는 숫자만 입력 가능합니다.";
+    private static final String WRONG_INPUT_DATE_ERROR_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
     private static final String BLANK = "";
     private static final int MIN_VISIT_DATE = 1;
     private static final int MAX_VISIT_DATE = 31;
 
     public void validateInputExpectedVisitDate(String visitDate) {
+        validateNonIntegerVisitDate(visitDate);
         validateNonInputVisitDate(visitDate);
         validateWrongRangeVisitDate(visitDate);
-        validateNonIntegerVisitDate(visitDate);
     }
 
     public void validateNonInputVisitDate(String visitDate) {
@@ -24,7 +23,7 @@ public class InputVisitDateValidator {
         int date = Integer.parseInt(visitDate);
 
         if (date < MIN_VISIT_DATE || date > MAX_VISIT_DATE) {
-            throw new IllegalArgumentException(WRONG_RANGE_ERROR_MESSAGE);
+            throw new IllegalArgumentException(WRONG_INPUT_DATE_ERROR_MESSAGE);
         }
     }
 
@@ -32,7 +31,7 @@ public class InputVisitDateValidator {
         try {
             Integer.parseInt(visitDate);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(NON_INTEGER_ERROR_MESSAGE);
+            throw new IllegalArgumentException(WRONG_INPUT_DATE_ERROR_MESSAGE);
         }
     }
 }
