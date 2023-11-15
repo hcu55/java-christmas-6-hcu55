3d57bc62c8e95b906da613ec33c1cbf47c6903fa
ChanUi <hcu55@naver.com>
2023-11-15 13:22:04 +0900

    fix: <혜택 내역>, <총혜택 금액> 출력 기능 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,12 +12,12 @@ public class OutputView {
     private static final String PRINT_EXPECTED_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String PRINT_ORDER_MENU_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static final String PRINT_PREVIEW_EVENT_BENEFIT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
-    private static final String PRINT_ORDER_MENU_AND_COUNT_MESSAGE = "<주문 메뉴>";
-    private static final String PRINT_TOTAL_ORDER_PRICE_MESSAGE = "\n<할인 전 총주문 금액>\n%s원";
-    private static final String PRINT_CHAMPAGNE_GIVEAWAY_MESSAGE = "\n<증정 메뉴>\n%s";
-    private static final String PRINT_DISCOUNT_DETAILS_MESSAGE = "\n<혜택 내역>\n%s";
-    private static final String PRINT_TOTAL_BENEFIT_MESSAGE = "<총혜택 금액>\n-%,d원\n";
-    private static final String PRINT_AFTER_DISCOUNT_PAYMENT_MESSAGE = "\n<할인 후 예상 결제 금액>\n%s원";
+    private static final String PRINT_ORDER_MENU_AND_COUNT_MESSAGE = "\n<주문 메뉴>";
+    private static final String PRINT_TOTAL_ORDER_PRICE_MESSAGE = "\n<할인 전 총주문 금액>\n%s원\n";
+    private static final String PRINT_CHAMPAGNE_GIVEAWAY_MESSAGE = "\n<증정 메뉴>\n%s\n";
+    private static final String PRINT_DISCOUNT_DETAILS_MESSAGE = "\n<혜택 내역>\n%s\n";
+    private static final String PRINT_TOTAL_BENEFIT_MESSAGE = "\n<총혜택 금액>\n%s원\n";
+    private static final String PRINT_AFTER_DISCOUNT_PAYMENT_MESSAGE = "\n<할인 후 예상 결제 금액>\n%s원\n";
     private static final String PRINT_EVENT_BADGE_MESSAGE = "\n<12월 이벤트 배지>\n%s";
     private DecimalFormat formatter = new DecimalFormat("#,###");
 
@@ -56,13 +56,16 @@ public class OutputView {
         System.out.printf(PRINT_CHAMPAGNE_GIVEAWAY_MESSAGE, "없음");
     }
 
-    public void printDiscountDetails(ChristmasEvent event) {
+    public void printDiscountDetails(ChristmasEvent event, boolean isChampagneGiveaway) {
         StringBuilder sb = new StringBuilder();
         event.processDiscountDetails((key, value) -> {
-            sb.append(key).append(": -").append(formatter.format(value)).append("원");
+            sb.append(key).append(": -").append(formatter.format(value)).append("원\n");
         });
+        if (isChampagneGiveaway) {
+            sb.append("증정 이벤트: -25,000원");
+        }
         if (sb.length() == 0) {
-            System.out.println("없음");
+            System.out.printf(PRINT_DISCOUNT_DETAILS_MESSAGE, "없음");
             return;
         }
         System.out.printf(PRINT_DISCOUNT_DETAILS_MESSAGE, sb.toString());
@@ -76,6 +79,9 @@ public class OutputView {
         int totalBenefit = eventBadge.totalBenefitAmount(totalDiscount, isChampagneGet);
 
         String formattedTotalBenefit = formatter.format(totalBenefit);
+        if (totalBenefit != 0) {
+            formattedTotalBenefit = "-" + formattedTotalBenefit;
+        }
         System.out.printf(PRINT_TOTAL_BENEFIT_MESSAGE, formattedTotalBenefit);
     }
 
